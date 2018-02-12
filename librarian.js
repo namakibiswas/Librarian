3138ea2d7490b27fd10212e47a2c6d2eaf0eaaf8
Namaki Biswas <1521939@iub.edu.bd>
2018-02-12 12:50:47 +0600

    Library

--- librarian.js
+++ librarian.js
@@ -3,7 +3,8 @@ var library = [
   { title: "The Old Man and the Sea", year: "1952", author: "Ernest Hemingway", publisher: "Charles Scribner's Sons" },
   { title: "Mythos: A Retelling of the Myths of Ancient Greece", year: "1981", author: "Stephen Fry", publisher: "Penguin" },
   { title: "The Mythical Man-Month", year: "1975", author: "Fred Brooks", publisher: "Addison-Wesley" },
-  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" }
+  { title: "The Pragmatic Programmer", year: "1999", author: "Andy Hunt and Dave Thomas", publisher: "The Pragmatic Bookshelf	" },
+  {title:  "Computer Networking", year:"2003", author: "Kurose", publisher:"Pearson"}
   /* Task 2 */
 ];
 
@@ -19,26 +20,50 @@ console.log(getBookTitle(3));
 function addBook( book ){
   library.push(book);
   return library;
-  //add code
 }
+var newBook = {title:"Kakababu Samagra", year:"1994", author: "Sunil Gangopadhyay", publisher:"Ananda"};
 
-var newBook = {};
 console.log(addBook(newBook));
 
-myarray.push({
-    "title": 'Creniel',
-    "year": '2012'
-    "Author": 'Muhammad Jafar Iqbal'
-});
+
+
+/*Task 4 */
+
 function bookByAuthor(){
-  //add code
+  var k = library.length;
+  var temp;
+  for (var i = 0; i <= k-1; i++) {
+    for (var j = 0; j <= k-1; j++) {
+      if (library[i].author < library[j].author) {
+        temp = library[i];
+        library[i] = library[j];
+        library[j] = temp;
+      }
+    }
+  }
+  return library;
+
 }
 
 console.log(bookByAuthor());
 
 /* Task 5 */
+
 function findByTitle( title ){
-  //add code
+
+  SearchedArray = [];
+
+  var x = new RegExp(title, 'i');
+
+  for (var i = library.length - 1; i >= 0; i--) {
+    if(x.test(library[i].title))
+    {
+      SearchedArray.push(library[i]);
+    }
+  }
+  return SearchedArray;
+
+
 }
 
 console.log(findByTitle("man"));
